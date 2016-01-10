a2bf9c1c9ab6a35f83a49fa09c293bdd6f576071
Manaia Junior <dev.manaiajr@gmail.com>
2016-01-10 18:23:58 -0300

    Atualiza urls para 1.9.1

--- spedanalytics/urls.py
+++ spedanalytics/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from spedanalytics.core.views import home
 
 urlpatterns = [
-    url(r'^$', 'spedanalytics.core.views.home'),
+    url(r'^$', home),
     url(r'^admin/', include(admin.site.urls)),
 ]
